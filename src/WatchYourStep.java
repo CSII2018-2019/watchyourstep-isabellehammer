f7eaf89012283c0e8caaa318b9388ce8be4753dc
Isabelle <hammeri@theellisschool.org>
2019-01-27 16:27:35 -0500

    _

--- src/WatchYourStep.java
+++ src/WatchYourStep.java
@@ -2,10 +2,12 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame; //might be able to delete
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.UIManager;
 
@@ -44,10 +46,115 @@ public class WatchYourStep extends JFrame {
 		for (int r = 0; r < GRIDSIZE; r++) {
 			for (int c = 0; c < GRIDSIZE; c++) {
 				terrain[r][c] = new TerrainButton(r, c);
+				terrain[r][c].addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						TerrainButton button = (TerrainButton) e.getSource();
+						int row = button.getRow();
+						int col = button.getCol();
+						clickedTerrain(row, col);
+					}
+				});
 				centerPanel.add(terrain[r][c]);
 			}
 		}
 		
+		private void clickedTerrain(int row, int col) {
+			if(!terrain[row][col].hasHole) {
+				String message = "You stepped in a hole :( TRY AGAIN";
+				promptForNewGame(message);
+			} else {
+				check(row, col);
+				checkNeighbors(row, col);
+				if (GRIDSIZE - NUMBEROFHOLES = 0) {
+					String message = "Congratulations! You won!";
+					promptForNewGame(message);
+				}
+			}
+			
+		}
+		
+		private void promptForNewGame(String mess) {
+			showHoles();
+			int option = JOptionPane.showConfirmDialog(this, mess, "Play again?", JOptionPane.YES_NO_OPTION);
+			if (option == JOptionPane.YES_OPTION) {
+				newGame();
+			} else {
+				System.exit(0);
+			}
+			
+			private void showHoles() {
+				for (_____) {
+					for (_____) {
+						if (!terrain[row][col].hasHole) {
+							terrain[row][col].reveal(true); //???
+						}
+					}
+				}
+			}
+		}
+		
+		private void newGame() {
+			for //NOT SURE WHAT TO DO HERE
+			setHoles(); //???
+			totalRevealed = 0;
+		}
+		
+		private void check(int row, int col) {
+			if (row > -1 && row < GRIDSIZE && col > -1 && col < GRIDSIZE && !terrain[row][col].hasHole() && !terrain[row][col.isRevealed()) {
+				terrain[row][col].reveal(true);
+				totalRevealed; //?????
+				if (!terrain[row][col].isNextToHoles()) {
+					checkNeighbors(row, col);
+				}
+			}
+		}
+		
+		private void checkNeighbors(int row, int col) {
+			check(row -1, col -1);
+			check(row -1, col);
+			check(row, col -1);
+			check(row -1, col +1);
+			check(row, col +1);
+			check(row +1, col +1);
+			check(row +1, col);
+			check(row +1, col -1);
+		}
+		
+		private void setHoles() {
+			Random rand = new Random();
+			int pickRow;
+			int pickCol;
+			for (int i = 0; i < NUMBEROFHOLES; i++) {
+				do {
+					pickRow = rand.nextInt(GRIDSIZE);
+					pickCol = rand.nextInt(GRIDSIZE);
+				} while (terrain[pickRow][pickCol].hasHole());
+				terrain[pickRow][pickCol].setHole(true);
+				addToNeighborsHoleCount(pickRow, pickCol);
+				terrain[pickRow][pickCol].reveal(true);
+			}
+		}
+		
+	}
+	
+	private void addToNeighborsHoleCount(int row, int col) {
+		addToHoleCount(row -1, col -1);
+		addToHoleCount(row -1, col);
+		addToHoleCount(row, col -1);
+		addToHoleCount(row -1, col +1);
+		addToHoleCount(row, col +1);
+		addToHoleCount(row +1, col +1);
+		addToHoleCount(row +1, col);
+		addToHoleCount(row +1, col -1);
+		
+	}
+	
+	private void addToHoleCount(int row, int col) {
+		if (row > -1 && row < GRIDSIZE && col > -1 && col < GRIDSIZE) {
+			terrain[row][col].increaseHoleCount();
+			terrain[row][col].reveal(true);
+		}
 	}
 	
 	
